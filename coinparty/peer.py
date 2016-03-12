bb67cb69c74e1e5d14a68bd57b13232ffe99d779
Max Fang <maxfangx@gmail.com>
2016-03-11 20:15:58 -0800

    Basic framework

---
+++ coinparty/peer.py
@@ -0,0 +1,10 @@
+class State:
+
+    def __init__(self):
+        self.mixing_peers = set()
+        self.input_peers = set()
+
+
+def start():
+
+    return State()
---
+++ coinparty/protocol.py
@@ -0,0 +1,8 @@
+def c1_generate_addresses(state):
+    updated_state = None
+    return updated_state
+
+
+def c2_compute_local_share_of_pub_key(state):
+    updated_state = None
+    return updated_state
